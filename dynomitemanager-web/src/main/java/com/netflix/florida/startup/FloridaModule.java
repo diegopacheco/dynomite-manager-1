4f342765ab1323b796510cfa548d0c53cf231400
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-10-24 14:20:15 -0700

    Fix Archaius prop sources

--- dynomitemanager-web/src/main/java/com/netflix/florida/startup/FloridaModule.java
+++ dynomitemanager-web/src/main/java/com/netflix/florida/startup/FloridaModule.java
@@ -12,6 +12,17 @@
  */
 package com.netflix.florida.startup;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.inject.Singleton;
+
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+
+import com.google.common.collect.Maps;
 import com.google.inject.AbstractModule;
 // Common module dependencies
 import com.google.inject.Provides;
@@ -21,7 +32,10 @@ import com.netflix.archaius.DefaultDecoder;
 import com.netflix.archaius.api.Config;
 import com.netflix.archaius.api.Decoder;
 import com.netflix.archaius.api.PropertyFactory;
+import com.netflix.archaius.api.exceptions.ConfigException;
 import com.netflix.archaius.config.DefaultCompositeConfig;
+import com.netflix.archaius.config.MapConfig;
+import com.netflix.archaius.config.SystemConfig;
 import com.netflix.dynomitemanager.FloridaServer;
 import com.netflix.dynomitemanager.aws.S3Backup;
 import com.netflix.dynomitemanager.aws.S3Restore;
@@ -35,12 +49,6 @@ import com.netflix.dynomitemanager.monitoring.JedisFactory;
 import com.netflix.dynomitemanager.monitoring.SimpleJedisFactory;
 import com.netflix.dynomitemanager.storage.RedisStorageProxy;
 import com.netflix.dynomitemanager.storage.StorageProxy;
-
-import javax.inject.Singleton;
-
-import org.quartz.SchedulerFactory;
-import org.quartz.impl.StdSchedulerFactory;
-
 import com.netflix.nfsidecar.archaius.DefaultPropertyFactory;
 import com.netflix.nfsidecar.aws.AWSMembership;
 import com.netflix.nfsidecar.aws.AwsInstanceEnvIdentity;
@@ -117,11 +125,47 @@ public final class FloridaModule extends AbstractModule {
         // bind(HostSupplier.class).to(EurekaHostsSupplier.class);
         
         /* Archaius */
-        bind(Config.class).to(DefaultCompositeConfig.class);
         bind(Decoder.class).to(DefaultDecoder.class);
         bind(PropertyFactory.class).to(DefaultPropertyFactory.class);
         
     }
+    
+	@Provides
+	@Singleton
+	Config getConfigs() {
+		Map<String, String> data = Maps.newConcurrentMap();
+		Map<String, String> env = System.getenv();
+		
+		for (String key : env.keySet()) {
+			String newKey = key.replaceAll("_", ".");
+			data.put("app." + newKey, env.get(key));
+		}
+		
+		
+        Properties properties = new Properties();
+        URL url = this.getClass().getClassLoader().getResource("florida.properties");
+        if (url != null) {
+            try {
+                properties.load(url.openStream());
+            } 
+            catch (IOException e) {
+            	e.printStackTrace();
+            }
+        } else {
+            System.out.println("No florida.properties. Ignore!");
+        }
+		
+		DefaultCompositeConfig config = new DefaultCompositeConfig();
+		try {
+			config.addConfig("system", new SystemConfig());
+			config.addConfig("envs",  new MapConfig(data));
+			config.addConfig("props", new MapConfig(properties));
+			
+		} catch (ConfigException e) {
+			e.printStackTrace();
+		}
+		return config;
+	}
 
     @Provides
     @Singleton
